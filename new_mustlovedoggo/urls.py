501ed58beaa95145eb33937398394518d1a7190c
AshleyMatula <ashleysmatula@gmail.com>
2019-09-12 19:31:09 -0400

    added formview

--- new_mustlovedoggo/urls.py
+++ new_mustlovedoggo/urls.py
@@ -18,8 +18,32 @@ from django.urls import path
 from django.views.generic import TemplateView
 class Home(TemplateView):
      template_name = "index.html"
+from django.views.generic import FormView
+from django import forms
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/',Home.as_view()),
+    path('',Home.as_view()),
 ]
+
+
+class NameForm(forms.Form):
+          your_name = forms.CharField(label='Your name', max_length=100)
+          first_name = forms.CharField(label='Your name', max_length=100)
+          last_name = forms.CharField(label='Your name', max_length=100)
+
+class FormClass(FormView):
+        template_name = "index.html"
+        form_class = NameForm
+
+        def post(self, request, *args, **kwargs):
+          """
+          Handle POST requests: instantiate a form instance with the passed
+          POST variables and then check if it's valid.
+          """
+          form = self.get_form()
+          print(form)
+          if form.is_valid():
+              return self.form_valid(form)
+          else:
+              return self.form_invalid(form)
